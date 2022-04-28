c958b140440c0890ddc757257b63aa821ebea3e9
Castard Allan <castard75@gmail.com>
2022-04-28 21:50:04 +0200

    finalisation projet

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -3,15 +3,15 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
 const multer = require("../middleware/multer-config");
-const checkSauceInput = require("../middleware/sauceInput");
+
 //Importation de notre controller pour le sauces
 const sauceCtrl = require("../controllers/sauce");
 
 //on fait l'authentification puis on appel la sauce ou les sauces
 router.get("/", auth, sauceCtrl.getAllSauce);
-router.post("/", auth, multer, checkSauceInput, sauceCtrl.createSauce);
+router.post("/", auth, multer, sauceCtrl.createSauce);
 router.get("/:id", auth, sauceCtrl.getOneSauce);
-router.put("/:id", auth, multer, checkSauceInput, sauceCtrl.updateSauce);
+router.put("/:id", auth, multer, sauceCtrl.updateSauce);
 router.delete("/:id", auth, sauceCtrl.deleteSauce);
 router.post("/:id/like", auth, sauceCtrl.likeSauce);
 module.exports = router;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const emailCheck = require("../middleware/verifyEmail");
-const passwordCheck = require("../middleware/passwordValidation");
+
 const userCtrl = require("../controllers/user");
 
-router.post("/signup", emailCheck, passwordCheck, userCtrl.signup);
+router.post("/signup", userCtrl.signup);
 router.post("/login", userCtrl.login);
 
 module.exports = router;
